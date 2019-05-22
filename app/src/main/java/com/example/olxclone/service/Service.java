e8b092f3c4123b4e52a15dca12dba0dd7b37945e
Jailson Silva <jjsj2@cin.ufpe.br>
2019-05-22 11:34:53 -0300

    obtendo as categorias

--- app/src/main/java/com/example/olxclone/service/Service.java
+++ app/src/main/java/com/example/olxclone/service/Service.java
@@ -1,5 +1,29 @@
 package com.example.olxclone.service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Service {
-    
+
+    private List<String> categories;
+
+    public List<String> getListCategories(){
+
+        createCategories();
+        return this.categories;
+    }
+
+    private void createCategories(){
+
+        this.categories = new ArrayList<>();
+        String[] array = {"Auto e peças", "Imóveis", "Eletrônicos e celulares",
+        "Para a sua casa", "Moda e beleza", "Esportes e lazer", "Música e hobbies",
+        "Artigos infantis", "Animais de estimação", "Agro e indústria", "Comécio e escritório",
+        "Serviçoes", "Vagas de emprego"};
+
+        for(int i = 0; i < array.length; i++){
+
+            this.categories.add(array[i]);
+        }
+    }
 }
